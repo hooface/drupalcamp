d6ab11f5f10e95891f19c29f6c66c6596ed5d352
swentel <swentel@realize.be>
2013-08-01 17:27:15 +0200

    Add information - make menu images smaller.

--- DrupalCampLeuven/DrupalCampLeuven/src/main/java/be/drupalcamp/leuven/Information.java
+++ DrupalCampLeuven/DrupalCampLeuven/src/main/java/be/drupalcamp/leuven/Information.java
@@ -1,6 +1,9 @@
 package be.drupalcamp.leuven;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class Information extends BaseActivity {
 
@@ -12,8 +15,15 @@ public class Information extends BaseActivity {
         // Set header title.
         setTextViewString(R.id.header_title, R.string.menu_info);
 
+        // Set information text. We're doing this programmatically so we can
+        // use HTML in the strings and make them behave what they are made for.
+        String text = (String)getResources().getText(R.string.information_text);
+        TextView t = (TextView) findViewById(R.id.information_text);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
+        t.setText(Html.fromHtml(text));
+
         // Set fonts.
         setFontToOpenSansLight(R.id.header_title);
-
+        setFontToOpenSansRegular(R.id.information_text);
     }
 }
